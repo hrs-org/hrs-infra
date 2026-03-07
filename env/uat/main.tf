a27d6f68776ec3abc12c454a73cfc4077c193bcd
kritty04 <krit1_c@hotmail.com>
2026-03-07 21:17:18 +0800

    change vm size to Standard_B2s_v2

--- env/uat/main.tf
+++ env/uat/main.tf
@@ -71,7 +71,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name            = "systemnp"
     node_count      = 1
-    vm_size         = "Standard_B2s"
+    vm_size         = "Standard_B2s_v2"
     os_disk_size_gb = 64
     upgrade_settings {
       max_surge                     = "10%"
